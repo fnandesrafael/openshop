c06106bb783cb13f1cdc77d98989de1145cd508d
fnandesrafael <fnandesrafael@pm.me>
2023-09-28 13:27:30 -0300

    fix: Header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,18 +4,18 @@ import { AiOutlineShoppingCart } from 'react-icons/ai';
 export default function Header() {
   return (
     <header
-      className="flex h-[4.5rem] w-full flex-row items-center justify-between bg-gray-200 p-6 shadow-sm"
+      className="flex h-24 w-full flex-row items-center justify-between bg-gray-200 p-8 shadow-sm"
       role="header"
     >
-      <h1 className="text-lg font-semibold" role="heading">
+      <h1 className="text-xl font-semibold" role="heading">
         openshop
       </h1>
 
       <button
-        className="flex h-10 w-10 flex-col items-center justify-center rounded-full border-[1px] border-solid border-black transition-all ease-in-out hover:bg-black hover:text-slate-200 hover:transition-all"
+        className="flex h-12 w-12 flex-col items-center justify-center rounded-full border-[1px] border-solid border-black transition-all ease-in-out hover:bg-black hover:text-slate-200 hover:transition-all"
         role="button"
       >
-        <AiOutlineShoppingCart className="mr-[2px] text-lg" />
+        <AiOutlineShoppingCart className="mr-[2px] text-xl" />
       </button>
     </header>
   );
