4bca36f378786d56dc02698f00213ec4bae9c648
fnandesrafael <fnandesrafael@pm.me>
2023-09-28 14:46:57 -0300

    feat: add styles to Cart header component and children

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BsArrowRightShort } from 'react-icons/bs';
 
 export default function Cart() {
   return (
@@ -6,9 +7,19 @@ export default function Cart() {
       className="fixed right-0 top-0 h-screen w-full bg-white sm:w-[50%] md:w-[40%] lg:w-[30%]"
       role="tab"
     >
-      <header role="header">
-        <h1 role="heading">Cart Items</h1>
-        <button role="button">Close</button>
+      <header
+        className="flex h-16 w-full flex-row items-center justify-between p-6 shadow-sm"
+        role="header"
+      >
+        <h1 className="text-lg" role="heading">
+          Cart Items
+        </h1>
+        <button
+          className="rounded-full p-1 transition-all ease-in-out  hover:bg-slate-50"
+          role="button"
+        >
+          <BsArrowRightShort className="text-3xl" />
+        </button>
       </header>
     </aside>
   );
