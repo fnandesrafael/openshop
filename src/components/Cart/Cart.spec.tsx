1707c68efc9405f8a452446f022710e8f7e81e92
fnandesrafael <fnandesrafael@pm.me>
2023-09-28 14:56:54 -0300

    feat: tdd new test case, component unmount on click

--- src/components/Cart/Cart.spec.tsx
+++ src/components/Cart/Cart.spec.tsx
@@ -1,11 +1,11 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Cart from './Cart';
 
 describe('<Cart/> component', () => {
   it('should be in the document', () => {
     const { getByRole } = render(<Cart />);
-
     const sut = getByRole('tab');
 
     expect(sut).toBeInTheDocument();
@@ -13,7 +13,6 @@ describe('<Cart/> component', () => {
 
   it('should have a header with a "Cart Items" heading, and a close button', () => {
     const { getByRole } = render(<Cart />);
-
     const [sutA, sutB, sutC] = [
       getByRole('header'),
       getByRole('heading'),
@@ -23,5 +22,14 @@ describe('<Cart/> component', () => {
     expect(sutA).toBeInTheDocument();
     expect(sutB).toBeInTheDocument();
     expect(sutC).toBeInTheDocument();
+  });
+
+  it("when the close button is clicked, it shouldn't be anymore in the document", () => {
+    const { getByRole } = render(<Cart />);
+    const sut = getByRole('button');
+
+    userEvent.click(sut);
+
+    expect(sut).not.toBeInTheDocument();
   });
 });
