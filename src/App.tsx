de75eb46ac318404a151865c281aeb56bf26b03f
fnandesrafael <fnandesrafael@pm.me>
2023-09-29 08:17:13 -0300

    feat: framer-motion and Cart component presence animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { AnimatePresence } from 'framer-motion';
 import Header from './components/Header';
 import Cart from './components/Cart';
 
@@ -9,7 +10,9 @@ function App() {
     <div className="h-screen w-full bg-gray-200" role="application">
       <Header setCanShowCart={setCanShowCart} />
 
-      {canShowCart && <Cart setCanShowCart={setCanShowCart} />}
+      <AnimatePresence mode="wait">
+        {canShowCart && <Cart setCanShowCart={setCanShowCart} />}
+      </AnimatePresence>
     </div>
   );
 }
