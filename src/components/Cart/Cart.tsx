d90378521c161579d8d82b3cea580091b44aa775
fnandesrafael <fnandesrafael@pm.me>
2023-09-30 15:31:16 -0300

    fix: Hero height and Cart span bg

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -8,31 +8,40 @@ type CartProps = {
 
 function Cart({ setCanShowCart }: CartProps) {
   return (
-    <motion.aside
-      initial={{ x: 500 }}
-      animate={{ x: 0 }}
-      exit={{ x: 500 }}
-      transition={{ damping: 25, type: 'spring', stiffness: 250 }}
-      className="fixed right-0 top-0 z-20 h-screen w-full origin-right bg-white sm:w-[50%] md:w-[40%] lg:w-[30%]"
-      role="tab"
-    >
-      <header
-        className="flex h-16 w-full flex-row items-center justify-between p-6 shadow-sm"
-        role="header"
+    <>
+      <motion.aside
+        initial={{ x: 425 }}
+        animate={{ x: 0 }}
+        exit={{ x: 425 }}
+        transition={{ damping: 25, type: 'spring', stiffness: 250 }}
+        className="fixed right-0 top-0 z-20 h-screen w-full origin-right bg-white sm:w-[50%] md:w-[40%] lg:w-[30%]"
+        role="tab"
       >
-        <h1 className="text-lg" role="heading">
-          Cart Items
-        </h1>
-
-        <button
-          className="rounded-full p-1 transition-all ease-in-out  hover:bg-slate-50"
-          role="close-btn"
-          onClick={() => setCanShowCart(false)}
+        <header
+          className="flex h-16 w-full flex-row items-center justify-between p-6 shadow-sm"
+          role="header"
         >
-          <BsArrowRightShort className="text-3xl" />
-        </button>
-      </header>
-    </motion.aside>
+          <h1 className="text-lg" role="heading">
+            Cart Items
+          </h1>
+
+          <button
+            className="rounded-full p-1 transition-all ease-in-out  hover:bg-slate-50"
+            role="close-btn"
+            onClick={() => setCanShowCart(false)}
+          >
+            <BsArrowRightShort className="text-3xl" />
+          </button>
+        </header>
+      </motion.aside>
+
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 0.25 }}
+        exit={{ opacity: 0 }}
+        className="fixed z-10 h-full w-full bg-black opacity-10"
+      />
+    </>
   );
 }
 
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,10 +3,13 @@ import stockVideo from '../../assets/stock.mp4';
 
 export default function Hero() {
   return (
-    <section className="relative mb-12 w-full overflow-hidden" role="hero">
+    <section
+      className="relative mb-12 h-[52vw] w-full overflow-hidden"
+      role="hero"
+    >
       <span className="absolute h-full w-full bg-black opacity-40" />
 
-      <h1 className="absolute left-16 top-36 w-4/6 font-serif text-8xl font-extralight leading-tight tracking-wide text-white">
+      <h1 className="absolute left-[4vw] top-[10vw] w-4/6 font-serif text-[8vw] font-extralight leading-tight tracking-wide text-white">
         Mussum ipsum cacilds vidis litro abertis.
       </h1>
 
