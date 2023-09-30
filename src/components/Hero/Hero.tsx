a55f54859cf50c39cab82beed1ca7d265a1aad45
fnandesrafael <fnandesrafael@pm.me>
2023-09-30 16:09:38 -0300

    fix: some style

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import stockVideo from '../../assets/stock.mp4';
 import ScrollTip from '../ScrollTip';
 
@@ -10,9 +11,13 @@ export default function Hero() {
     >
       <span className="absolute h-full w-full bg-black opacity-40" />
 
-      <h1 className="absolute left-[4vw] top-[10vw] w-4/6 font-serif text-[8vw] font-extralight leading-tight tracking-wide text-white">
+      <motion.h1
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { duration: 4, delay: 0.6 } }}
+        className="absolute left-[4vw] top-[10vw] w-4/6 font-serif text-[8vw] font-extralight leading-tight tracking-wide text-white"
+      >
         Mussum ipsum cacilds vidis litro abertis.
-      </h1>
+      </motion.h1>
 
       <video className="w-full" src={stockVideo} loop autoPlay muted />
 
