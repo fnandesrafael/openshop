419b8c09273adf7a52b3e4b8443858abcd320869
fnandesrafael <fnandesrafael@pm.me>
2023-10-07 18:10:23 -0300

    fix: Spinner icon

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+import { AiOutlineLoading } from 'react-icons/ai';
 
 export default function Spinner() {
   return (
     <motion.span className="flex flex-col items-center justify-center">
-      <AiOutlineLoading3Quarters className="mx-4 animate-spin text-base text-black duration-500" />
+      <AiOutlineLoading className="mx-4 animate-spin text-xl text-black duration-500" />
     </motion.span>
   );
 }
