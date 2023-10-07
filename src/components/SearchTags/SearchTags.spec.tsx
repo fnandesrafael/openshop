e71b1c2cc3a55e3b50a5ed3f69f661dce1590957
fnandesrafael <fnandesrafael@pm.me>
2023-10-07 10:27:34 -0300

    fix: SearchTags.spec.tsx

--- src/components/SearchTags/SearchTags.spec.tsx
+++ src/components/SearchTags/SearchTags.spec.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { fireEvent, render } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import SearchTags from './SearchTags';
 
 const mockedSetState = jest.fn();
+const mockedCategories = Array.from({ length: 4 });
+
+jest.mock('react-query', () => ({
+  ...jest.requireActual('react-query'),
+  useQuery: jest.fn(() => ({ data: mockedCategories })),
+}));
 jest.mock('react', () => ({
   ...jest.requireActual('react'),
   useState: () => [{}, mockedSetState],
 }));
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { refetchOnWindowFocus: false, refetchInterval: 0 },
+  },
+});
+
 describe('<SearchTag/> component', () => {
   it('should be in the document', () => {
-    const { getAllByRole } = render(<SearchTags />);
+    const { getAllByRole } = render(
+      <QueryClientProvider client={queryClient}>
+        <SearchTags />
+      </QueryClientProvider>,
+    );
     const sut = getAllByRole('searchbox');
 
     expect(sut).toHaveLength(4);
   });
 
   it('should call a setState SetState when checked', () => {
-    const { getAllByRole } = render(<SearchTags />);
+    const { getAllByRole } = render(
+      <QueryClientProvider client={queryClient}>
+        <SearchTags />
+      </QueryClientProvider>,
+    );
     const sut = getAllByRole('searchbox');
 
     fireEvent.click(sut[0]);
