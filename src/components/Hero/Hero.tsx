74f87069ef1f7d756cff26733fe601f05a054da3
fnandesrafael <fnandesrafael@pm.me>
2023-10-22 19:06:23 -0300

    fix: Hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -14,7 +14,7 @@ function Hero() {
       <motion.h1
         initial={{ opacity: 0 }}
         animate={{ opacity: 1, transition: { duration: 4, delay: 0.6 } }}
-        className="mobile-l:w-80 mobile-sm:w-48 mobile-m:w-56 mobile-m:text-4xl mobile-l:text-5xl absolute bottom-8 left-[5%] font-serif text-3xl font-extralight tracking-wide text-white md:bottom-10 md:w-[38rem] md:text-[4rem] md:leading-[1.2] xl:bottom-32 xl:w-[62rem] xl:text-[6.5rem] 2xl:w-[72rem] 2xl:text-[8rem]"
+        className="mobile-l:w-80 mobile-sm:w-[14rem] mobile-m:w-56 mobile-m:text-4xl mobile-sm:text-4xl mobile-l:text-5xl absolute bottom-8 left-[5%] font-serif text-3xl font-extralight tracking-wide text-white md:bottom-10 md:w-[38rem] md:text-[4rem] md:leading-[1.2] xl:bottom-32 xl:w-[62rem] xl:text-[6.5rem] 2xl:w-[72rem] 2xl:text-[8rem]"
       >
         Mussum ipsum cacilds vidis litro abertis.
       </motion.h1>
