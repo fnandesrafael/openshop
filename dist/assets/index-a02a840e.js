00c130406a6cfb5df588f79ebe99fe6656cac0a8
fnandesrafael <fnandesrafael@pm.me>
2023-10-22 15:39:05 -0300

    fix: tsconfig build file

---
+++ dist/assets/index-a02a840e.js
@@ -0,0 +1,1 @@
+import{P as r}from"./index-03049de1.js";import{b as a}from"./index-03049de1.js";export{a as Skeleton,r as default};
---
+++ dist/assets/index-dbf2df79.js
@@ -0,0 +1,1 @@
+import{S as e}from"./index-03049de1.js";import{a as t}from"./index-03049de1.js";export{t as Skeleton,e as default};
